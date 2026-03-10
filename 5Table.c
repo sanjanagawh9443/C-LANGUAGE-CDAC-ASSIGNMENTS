e33bb85add6944afbe49db87dad022ebc9adc32e
Sanjana Gawhande <141925971+sanjanagawh9443@users.noreply.github.com>
2026-03-10 16:59:56 +0530

    Add files via upload

---
+++ 5Table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=5;i<=10;i++)
+    {
+        for(j=1;j<=10;j++)
+        {
+            printf("%d*%d=%d \n", i ,j,i*j);
+        }
+        printf("----\n");
+    }
+}
