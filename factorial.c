aa084d2003ecac3ef1c1a76a369511d73cd8706a
Sanjana Gawhande <141925971+sanjanagawh9443@users.noreply.github.com>
2026-03-10 17:04:37 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,n;
+    int fact=1;
+    printf("display factorial 1 to 10:\n");
+    scanf("%d",&n);
+    {
+ for(i=1;i<=n;i++);
+
+   {
+    fact=fact*i;
+   }
+   
+        {
+            printf("factorial=%d",fact);
+        }
+        return 0;
+    }
+}
